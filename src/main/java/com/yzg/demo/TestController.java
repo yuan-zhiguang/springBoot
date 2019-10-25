964005b1ac702a346fc4946b88da12643abfb827
yuanzhiguang <yuan_zhiguang1@163.com>
2019-10-25 10:57:04 +0800

    add getMapping

--- src/main/java/com/yzg/demo/TestController.java
+++ src/main/java/com/yzg/demo/TestController.java
@@ -1,6 +1,7 @@
 package com.yzg.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class TestController {
     @RequestMapping(value = "/world", method = RequestMethod.GET)
     public String helloWorld(){
         return "Hello World !";
+    }
+	
+	@GetMapping("/world")
+    public String helloWorld2(){
+        return "Hello World2 !";
     }
 }
